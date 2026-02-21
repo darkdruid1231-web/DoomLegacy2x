412a92676975897b49b9d1f6c06525fa11de5976
Druid <druid@localhost>
2026-02-21 14:01:46 -0700

    fix: comprehensive TNL stub headers enable most tests to build
    - Add single comprehensive tnl_stub.h header
    - Create wrapper headers for tnl/ subdirectory includes
    - Fix z_zone.cpp includes
    - Fix m_fixed.cpp to use lnet::BitStream

--- legacy/trunk/include_stubs/tnl/tnlGhostConnection.h
+++ legacy/trunk/include_stubs/tnl/tnlGhostConnection.h
@@ -1,25 +1,2 @@
-#ifndef TNL_GHOST_CONNECTION_H
-#define TNL_GHOST_CONNECTION_H
-
-#include "tnlNetInterface.h"
-
-namespace TNL {
-
-class GhostConnection : public NetConnection {
-public:
-    GhostConnection() {}
-    virtual ~GhostConnection() {}
-    
-    // Ghost interface stubs
-    void addGhost(NetObject* obj) {}
-    void removeGhost(NetObject* obj) {}
-    void updateGhost(NetObject* obj) {}
-    
-    virtual void onGhostAdded(NetObject* obj) {}
-    virtual void onGhostRemoved(NetObject* obj) {}
-    virtual void onGhostUpdate(NetObject* obj) {}
-};
-
-} // namespace TNL
-
-#endif // TNL_GHOST_CONNECTION_H
+// Wrapper - include comprehensive stub
+#include "tnl_stub.h"
