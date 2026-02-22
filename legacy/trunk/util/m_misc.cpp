60f7577f6e004db38148b20086272778723d5085
Druid <druid@localhost>
2026-02-22 14:56:32 -0700

    Fix GCC 13+ / MinGW-w64: stronger header block with __CPUID_H
    Add both __XSAVEINTRIN_H and __CPUID_H to block GCC's
    conflicting intrinsic headers, plus pragma blocks for warnings

--- legacy/trunk/util/m_misc.cpp
+++ legacy/trunk/util/m_misc.cpp
@@ -1,16 +1,19 @@
-// Fix GCC 13+ / MinGW-w64 header conflict
+// $Id: m_misc.cpp 616 2010-03-23 05:15:47Z smite-meister $
+// Fix GCC 13+ / MinGW-w64 header conflict - stronger block
 #ifdef __MINGW32__
-// Block GCC's conflicting signed _xgetbv declaration by skipping xsaveintrin.h
+// Prevent GCC from including xsaveintrin.h (and its signed _xgetbv)
 #define __XSAVEINTRIN_H
-// Suppress __cpuidex redeclaration warning
+// Also block cpuid.h if needed (for __cpuidex)
+#define __CPUID_H
+// Suppress warnings
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wattributes"
-// Force MinGW-w64's intrinsics early (unsigned _xgetbv version)
+#pragma GCC diagnostic ignored "-Wredundant-decls"
+// Force MinGW intrin.h early
 #include <intrin.h>
 #pragma GCC diagnostic pop
 #endif
 
-// $Id: m_misc.cpp 616 2010-03-23 05:15:47Z smite-meister $
 //
 // Copyright (C) 1993-1996 by id Software, Inc.
 // Copyright (C) 1998-2007 by DooM Legacy Team.
