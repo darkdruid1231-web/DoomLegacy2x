a7c3a3483b012c050dd89fb9b6d4353c7fb64a51
Druid <druid@localhost>
2026-02-22 16:35:42 -0700

    Change to -Wno-missing-attributes to suppress the gnu_inline redeclaration warning

--- legacy/trunk/cmake/mingw-w64-x86_64.cmake
+++ legacy/trunk/cmake/mingw-w64-x86_64.cmake
@@ -26,7 +26,8 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 # Windows-specific link flags: bundle libgcc/libstdc++ statically so the
 # produced .exe doesn't require extra DLLs at runtime.
-set(CMAKE_CXX_FLAGS     "${CMAKE_CXX_FLAGS}     -static-libgcc -static-libstdc++ -Wno-attributes")
+set(CMAKE_C_FLAGS       "${CMAKE_C_FLAGS}       -Wno-missing-attributes")
+set(CMAKE_CXX_FLAGS     "${CMAKE_CXX_FLAGS}     -static-libgcc -static-libstdc++ -Wno-missing-attributes")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")
 
 # No RPATH on Windows
