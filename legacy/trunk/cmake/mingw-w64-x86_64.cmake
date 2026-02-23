9dbb34f877805ea74a97bc39c0bb69766b803b2b
Druid <druid@localhost>
2026-02-22 17:31:42 -0700

    Revert to GCC-based MinGW with -Wno-inline to suppress __cpuidex error

--- legacy/trunk/cmake/mingw-w64-x86_64.cmake
+++ legacy/trunk/cmake/mingw-w64-x86_64.cmake
@@ -4,14 +4,14 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-# Cross-compiler paths: use LLVM MinGW (clang-based)
-set(CMAKE_C_COMPILER   /usr/local/llvm-mingw/bin/x86_64-w64-mingw32-clang)
-set(CMAKE_CXX_COMPILER /usr/local/llvm-mingw/bin/x86_64-w64-mingw32-clang++)
+# Cross-compiler paths: use GCC MinGW
+set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc-posix)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-posix)
 set(CMAKE_RC_COMPILER  x86_64-w64-mingw32-windres)
 
-# Tell CMake's find_* commands to look inside the LLVM MinGW sysroot for libraries
+# Tell CMake's find_* commands to look inside the MinGW sysroot for libraries
 # and headers, and to never search the build-host programs.
-set(CMAKE_FIND_ROOT_PATH /usr/local/llvm-mingw)
+set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
@@ -29,7 +29,7 @@ set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")
 # No RPATH on Windows
 set(CMAKE_SKIP_RPATH TRUE)
 
-# pkg-config: point at the LLVM MinGW sysroot's .pc files.
+# pkg-config: point at the MinGW sysroot's .pc files.
 set(PKG_CONFIG_EXECUTABLE    /usr/bin/pkg-config)
-set(PKG_CONFIG_SYSROOT_DIR   /usr/local/llvm-mingw)
-set(PKG_CONFIG_PREFIX        /usr/local/llvm-mingw)
+set(PKG_CONFIG_SYSROOT_DIR   /usr/x86_64-w64-mingw32)
+set(PKG_CONFIG_PREFIX        /usr/x86_64-w64-mingw32)
