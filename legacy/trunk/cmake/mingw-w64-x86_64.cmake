ab6cd956c245baa2461e11ccc047fe3934a4cf25
Druid <druid@localhost>
2026-02-22 17:15:02 -0700

    Switch CI to use LLVM MinGW (clang-based) toolchain to avoid GCC 13 MinGW-w64 header conflicts

--- legacy/trunk/cmake/mingw-w64-x86_64.cmake
+++ legacy/trunk/cmake/mingw-w64-x86_64.cmake
@@ -4,17 +4,14 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-# Cross-compiler paths.
-# Use the posix-threading variant: the win32 variant ships with a crippled
-# C++ stdlib that breaks <ctime>/<chrono> (clock_t not in global namespace).
-# The posix variant is installed alongside win32 by the mingw-w64 package.
-set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc-posix)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-posix)
+# Cross-compiler paths: use LLVM MinGW (clang-based)
+set(CMAKE_C_COMPILER   x86_64-w64-mingw32-clang)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-clang++)
 set(CMAKE_RC_COMPILER  x86_64-w64-mingw32-windres)
 
-# Tell CMake's find_* commands to look inside the MinGW sysroot for libraries
+# Tell CMake's find_* commands to look inside the LLVM MinGW sysroot for libraries
 # and headers, and to never search the build-host programs.
-set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH /usr/local/llvm-mingw)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
@@ -32,8 +29,7 @@ set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")
 # No RPATH on Windows
 set(CMAKE_SKIP_RPATH TRUE)
 
-# pkg-config: point at the MinGW sysroot's .pc files.
-# (PKG_CONFIG_PATH env var is also set in the CI step for redundancy.)
+# pkg-config: point at the LLVM MinGW sysroot's .pc files.
 set(PKG_CONFIG_EXECUTABLE    /usr/bin/pkg-config)
-set(PKG_CONFIG_SYSROOT_DIR   /usr/x86_64-w64-mingw32)
-set(PKG_CONFIG_PREFIX        /usr/x86_64-w64-mingw32)
+set(PKG_CONFIG_SYSROOT_DIR   /usr/local/llvm-mingw)
+set(PKG_CONFIG_PREFIX        /usr/local/llvm-mingw)
