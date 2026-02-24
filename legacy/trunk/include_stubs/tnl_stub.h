0a36fc7482dba86c1ac99822d1b7a734281b08e4
Allanon <allanon@doomlegacy.dev>
2026-02-24 14:40:22 -0700

    Remove Actor class to avoid redefinition

--- legacy/trunk/include_stubs/tnl_stub.h
+++ legacy/trunk/include_stubs/tnl_stub.h
@@ -184,13 +184,6 @@ public:
     void setMaskBits(U32 mask) {}
 };
 
-class Actor : public NetObject {
-public:
-    virtual ~Actor() {}
-    virtual void packUpdate(GhostConnection* c, unsigned int mask, BitStream* s) {}
-    virtual void unpackUpdate(GhostConnection* c, BitStream* s) {}
-};
-
 class NetConnection {
 public:
     enum TerminationReason {
