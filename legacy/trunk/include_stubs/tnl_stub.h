0919ac3288a52d79a35ee8f9565f71efea89161e
Allanon <allanon@doomlegacy.dev>
2026-02-24 14:51:46 -0700

    Add stub implementations for RPCs via macro, computeClientIdentityToken, and SDL_mixer functions

--- legacy/trunk/include_stubs/tnl_stub.h
+++ legacy/trunk/include_stubs/tnl_stub.h
@@ -39,7 +39,7 @@ typedef BitStream* ByteBufferPtr;
 
 // Macros
 #define BIT(x) (1 << (x))
-#define TNL_DECLARE_RPC(func, params) void func params
+#define TNL_DECLARE_RPC(func, params) void func params {}
 #define TNL_IMPLEMENT_CLASS(cls)
 #define TNL_IMPLEMENT_NETOBJECT(cls)
 #define TNL_IMPLEMENT_NETCONNECTION(cls, group, flag)
@@ -274,6 +274,19 @@ public:
 };
 
 // Utility functions
-U32 computeClientIdentityToken(const Address& addr, const Nonce& nonce);
+U32 computeClientIdentityToken(const Address& addr, const Nonce& nonce) { return 0; }
+
+// Stub SDL_mixer functions
+int Mix_OpenAudio(int frequency, Uint16 format, int channels, int chunksize) { return 0; }
+int Mix_QuerySpec(int* frequency, Uint16* format, int* channels) { return 0; }
+void Mix_SetPostMix(void (*mix_func)(void *udata, Uint8 *stream, int len), void *arg) {}
+void Mix_Resume(int channel) {}
+void Mix_ResumeMusic() {}
+void Mix_CloseAudio() {}
+void Mix_FreeMusic(Mix_Music* music) {}
+Mix_Music* Mix_LoadMUS_RW(SDL_RWops* rw, int freesrc) { return nullptr; }
+int Mix_FadeInMusic(Mix_Music* music, int loops, int ms) { return 0; }
+int Mix_FadeOutMusic(int ms) { return 0; }
+int Mix_VolumeMusic(int volume) { return 0; }
 
 #endif // TNL_STUB_H
